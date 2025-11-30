be7800124373666537e29962b728fa1ac5b84ff4
Khaled Mashkoor <kmashkoo@c3r1p1.42vienna.com>
2025-11-30 18:22:48 +0100

    trying to write ex01

---
+++ cpp00/ex01/Contact.hpp
@@ -0,0 +1,21 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact
+{
+    private:
+        std::string _firstName;
+        std::string _firstName;
+        std::string _firstName;
+        std::string _firstName;
+        std::string _firstName;
+    
+    public:
+        Contact();
+        ~Contact();
+
+}
+
+#endif
